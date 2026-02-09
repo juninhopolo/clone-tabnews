52abbb1f0caebbd91fd23da945c1f1e4a474f77c
Junior Polo <32656150+juninhopolo@users.noreply.github.com>
2026-02-09 23:28:31 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do tabnews.com.br aprendida no curso.dev.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Comecei a fazer o curso! 😎🚀</h1>;
+  return <h1>Comecei a fazer o curso! 😎🚀</h1>;
 }
 
 export default Home;
